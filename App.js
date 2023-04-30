7494c8030af6a62aeb2b0fbb78fdcfa2fb219435
WillOli <william.ika@gmail.com>
2023-04-30 16:46:54 -0300

    adicionar gestos

--- App.js
+++ App.js
@@ -1,47 +1,78 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-import Button from './Button';
-import ImageViewer from './ImageViewer';
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from './components/EmojiPicker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
-import * as ImagePicker from 'expo-image-picker';
-
-
-
-
-
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
+      allowsEditing: true,      
       quality: 1,
     });
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
-  }  
+  };
 
-  return (    
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+
+  return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-      <ImageViewer
-          placeholderImageSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button
+            label="Use this photo"            
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* A list of emoji component will go here */}
+      </EmojiPicker>
       <StatusBar style="auto" />
-      
     </View>
   );
 }
@@ -53,16 +84,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+    flex:1, 
+    paddingTop: 58
   },
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,14 +1,13 @@
 import { StyleSheet, Image } from 'react-native';
 
 export default function ImageViewer({ placeholderImageSource, selectedImage }) {
-  const imageSource = selectedImage !== null
-    ? { uri: selectedImage }
-    : placeholderImageSource;
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
 
   return <Image source={imageSource} style={styles.image} />;
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  
   image: {
     width: 320,
     height: 440,
